4703ee16de655cfdc9709a748553fee350f98bcd
Anil Kumar Yadav <ak45yadv@4gmail.com>
2026-03-28 22:47:51 +0530

    fix port and cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 // app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
