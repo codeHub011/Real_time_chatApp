3475abbe31fa2065617ace686e1106085cd884cd
Anil Kumar Yadav <ak45yadv@4gmail.com>
2026-03-29 01:10:22 +0530

    fix cookie issue final

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://real-time-chat-app-npvo.vercel.app"
-    ],
+    origin: 
+      
+      "https://real-time-chat-app-npvo.vercel.app",
+    
     credentials: true,
   })
 );
